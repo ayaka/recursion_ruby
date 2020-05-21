e69501a9c48d10c2e5241e3301c370845062c611
threeaces <dev.threeaces@gmail.com>
2020-05-21 09:18:10 -0700

    refactoring

--- merge_sort.rb
+++ merge_sort.rb
@@ -4,22 +4,17 @@ def merge_sort (arr, sorted = [], i = 0, j = 0)
   arr1 = merge_sort(arr.slice(0...(arr.length / 2)))
   arr2 = merge_sort(arr.slice((arr.length / 2)...arr.length))
 
-  until sorted.length == (arr1.length + arr2.length)
-    if i >= arr1.length
-      sorted += arr2.slice(j...arr2.length)
-    elsif j >= arr2.length
-      sorted += arr1.slice(i...arr1.length)
+  until sorted.length == (arr1.length + arr2.length) 
+    return sorted + arr2.slice(j...arr2.length) if i >= arr1.length
+    return sorted + arr1.slice(i...arr1.length) if j >= arr2.length
+    if arr1[i] < arr2[j]
+      sorted << arr1[i]
+      i += 1
     else
-      if arr1[i] < arr2[j]
-        sorted << arr1[i]
-        i += 1
-      else
-        sorted << arr2[j]
-        j += 1
-      end
+      sorted << arr2[j]
+      j += 1
     end
   end
-  sorted
 end
 
 p arr_even = (1..10).to_a.shuffle
